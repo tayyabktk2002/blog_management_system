181a579334e98e448436a376855c13e637a41c05
tayyabktk2002 <tayyabktk2002@gmail.com>
2025-12-30 23:02:49 +0500

    project live set

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog.ahtelecom.cloud/api/v1/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
